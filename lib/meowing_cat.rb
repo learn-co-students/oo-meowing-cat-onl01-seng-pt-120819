d903574d4161be01a0ea4a44b637c4dcc04a7f0b
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-28 02:03:54 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+## code your solution here.
+require 'pry'
+class Cat
+  attr_accessor :name, #:meow
+  # "meow!"
+
+  def meow
+    puts "meow!"
+  end
+
+end
+binding.pry
